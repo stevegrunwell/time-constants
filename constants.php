bd269cff72db2dba3db815e751de79ac38fcfdac
Steve Grunwell <233836+stevegrunwell@users.noreply.github.com>
2023-12-17 12:30:07 -0500

    Remove the space between the logical not ("!") operator and `defined()`
    This is a change for me, but https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/issues/7303 makes a good point for `if (!defined())` being a better match with PSR-12 than `if(! defined())`.

--- constants.php
+++ constants.php
@@ -15,37 +15,37 @@
  */
 
 /* One second. */
-if (! defined('ONE_SECOND')) {
+if (!defined('ONE_SECOND')) {
     define('ONE_SECOND', 1);
 }
 
 /* One minute = 60 seconds. */
-if (! defined('MINUTE_IN_SECONDS')) {
+if (!defined('MINUTE_IN_SECONDS')) {
     define('MINUTE_IN_SECONDS', 60);
 }
 
 /* One hour = 60 minutes. */
-if (! defined('HOUR_IN_SECONDS')) {
+if (!defined('HOUR_IN_SECONDS')) {
     define('HOUR_IN_SECONDS', 60 * MINUTE_IN_SECONDS);
 }
 
 /* One day = 24 hours. */
-if (! defined('DAY_IN_SECONDS')) {
+if (!defined('DAY_IN_SECONDS')) {
     define('DAY_IN_SECONDS', 24 * HOUR_IN_SECONDS);
 }
 
 /* One week = 7 days. */
-if (! defined('WEEK_IN_SECONDS')) {
+if (!defined('WEEK_IN_SECONDS')) {
     define('WEEK_IN_SECONDS', 7 * DAY_IN_SECONDS);
 }
 
 /* For common usage, assume one month = 30 days. */
-if (! defined('MONTH_IN_SECONDS')) {
+if (!defined('MONTH_IN_SECONDS')) {
     define('MONTH_IN_SECONDS', 30 * DAY_IN_SECONDS);
 }
 
 /* For common usage, assume one year = 365 days. */
-if (! defined('YEAR_IN_SECONDS')) {
+if (!defined('YEAR_IN_SECONDS')) {
     define('YEAR_IN_SECONDS', 365 * DAY_IN_SECONDS);
 }
 
@@ -57,32 +57,32 @@ if (! defined('YEAR_IN_SECONDS')) {
  */
 
 /* One minute. */
-if (! defined('ONE_MINUTE')) {
+if (!defined('ONE_MINUTE')) {
     define('ONE_MINUTE', 1);
 }
 
 /* One hour = 60 minutes. */
-if (! defined('HOUR_IN_MINUTES')) {
+if (!defined('HOUR_IN_MINUTES')) {
     define('HOUR_IN_MINUTES', 60);
 }
 
 /* One day = 24 hours. */
-if (! defined('DAY_IN_MINUTES')) {
+if (!defined('DAY_IN_MINUTES')) {
     define('DAY_IN_MINUTES', 24 * HOUR_IN_MINUTES);
 }
 
 /* One week = 7 days. */
-if (! defined('WEEK_IN_MINUTES')) {
+if (!defined('WEEK_IN_MINUTES')) {
     define('WEEK_IN_MINUTES', 7 * DAY_IN_MINUTES);
 }
 
 /* For common usage, assume one month = 30 days. */
-if (! defined('MONTH_IN_MINUTES')) {
+if (!defined('MONTH_IN_MINUTES')) {
     define('MONTH_IN_MINUTES', 30 * DAY_IN_MINUTES);
 }
 
 /* For common usage, assume one year = 365 days. */
-if (! defined('YEAR_IN_MINUTES')) {
+if (!defined('YEAR_IN_MINUTES')) {
     define('YEAR_IN_MINUTES', 365 * DAY_IN_MINUTES);
 }
 
@@ -93,21 +93,21 @@ if (! defined('YEAR_IN_MINUTES')) {
  */
 
 /* A millisecond is 1/1000 of a second. */
-if (! defined('MILLISECONDS_PER_SECOND')) {
+if (!defined('MILLISECONDS_PER_SECOND')) {
     define('MILLISECONDS_PER_SECOND', 1000);
 }
 
 /* A microsecond is one millionth of a second. */
-if (! defined('MICROSECONDS_PER_SECOND')) {
+if (!defined('MICROSECONDS_PER_SECOND')) {
     define('MICROSECONDS_PER_SECOND', 1000000);
 }
 
 /* A nanosecond is one billionth of a second. */
-if (! defined('NANOSECONDS_PER_SECOND')) {
+if (!defined('NANOSECONDS_PER_SECOND')) {
     define('NANOSECONDS_PER_SECOND', 1000000000);
 }
 
 /* A picosecond is one trillionth of a second. */
-if (! defined('PICOSECONDS_PER_SECOND')) {
+if (!defined('PICOSECONDS_PER_SECOND')) {
     define('PICOSECONDS_PER_SECOND', 1000000000000);
 }
